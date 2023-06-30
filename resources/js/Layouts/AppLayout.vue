e91ee20f23ddf1873e502ef5ff6285e22ab4e9ff
Fideline <fideline_bzn@yahoo.com>
2023-06-30 13:49:06 +0200

    fixing notification button

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -77,11 +77,14 @@ const logout = () => {
                                     <Dropdown align="right" width="48">
                                         <template #trigger>
                                         <span>
-                                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent  font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent  font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                                                     <div class=" bg-red-500 absolute text-[8px] py-1 px-2 top-0 left-6 rounded-2xl text-white">25</div>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                                                 </svg>
+                                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                                    </svg>
                                                 </button>
                                             </span>
                                         </template>
