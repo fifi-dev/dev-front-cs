57219b0ecf16aa6d4f810e0c4638a114511d401b
Fideline <fideline_bzn@yahoo.com>
2023-06-30 20:25:19 +0200

    work sort data

--- resources/js/Pages/Materials.vue
+++ resources/js/Pages/Materials.vue
@@ -235,7 +235,7 @@ import DataCard from '@/Components/DataCard.vue';
                 </div>
                 <!-- Table with data -->
                 <div class="bg-white overflow-hidden overflow-x-scroll  shadow-xl sm:rounded-lg p-4 mt-4 mb-20">
-                    <div class="flex items-center pl-6">
+                    <div class="flex items-center pl-3">
                         <!-- select All-->
                         <Checkbox :value="selectAll"/>
                         <!-- Delete Button -->
@@ -254,63 +254,67 @@ import DataCard from '@/Components/DataCard.vue';
                                     <th  class="text-sm font-medium text-gray-900 px-3  py-4 text-left">
                                         <!-- Checkbox column -->
                                     </th>
-                                    <th  class="text-sm font-medium text-gray-900 px-3  flex items-center py-4 text-left">
-                                        Type de matériel <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                    <th class="text-sm font-medium text-gray-900 px-3 py-4 text-left">
+                                        <div class="flex items-center">
+                                            Type de matériel 
+                                            <svg @click="sort('type')" xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2 cursor-pointer w-3 h-3"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                        </div>
+                                        
                                     </th>
                                     <!--Rings Col-->
                                     <th  class="text-sm font-medium text-gray-900 px-3 py-4 text-left">
                                         <div class="flex items-center">
                                             Bagues
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                            <svg @click="sort('ring')" xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2 cursor-pointer w-3 h-3"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
                                         </div>
                                     </th>
                                     <!-- Sensors Col -->
                                     <th  class="text-sm font-medium text-gray-900 px-3  py-4 text-left">
-                                        <div class="flex items-center">
+                                        <div class="flex items-center" >
                                             Capteur
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                            <svg @click="sort('sensor')" xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2 cursor-pointer w-3 h-3"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
                                         </div>
                                     </th>
                                     <!-- Status Col-->
                                     <th  class="text-sm font-medium text-gray-900 px-3  py-4 text-left">
                                         <div class="flex items-center">
                                             Statut
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                            <svg  @click="sort('status')" xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2 cursor-pointer w-3 h-3"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
                                         </div>
                                     </th>
                                     <!-- Current Areas Col-->
                                     <th  class="text-sm font-medium text-gray-900 px-3  py-4 text-left">
                                         <div class="flex items-center">
                                             Zone actuelle
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                            <svg @click="sort('current_area')" xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2 cursor-pointer w-3 h-3"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
                                         </div>
                                     </th>
                                     <!-- Detection Col-->
                                     <th  class="text-sm font-medium text-gray-900 px-3  py-4 text-left">
                                         <div class="flex items-center">
                                             Détection
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                            <svg @click="sort('detection')" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="ml-2 cursor-pointer w-3 h-3"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
                                         </div>
                                     </th>
                                     <!-- Sites Col-->
                                     <th  class="text-sm font-medium text-gray-900 px-3  py-4 text-left">
                                         <div class="flex items-center">
                                             Site
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                            <svg @click="sort('site')" xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2 cursor-pointer w-3 h-3"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
                                         </div>
                                     </th>
                                     <!-- State Col-->
                                     <th  class="text-sm font-medium text-gray-900 px-3  py-4 text-left">
                                         <div class="flex items-center">
                                             État
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                            <svg @click="sort('state')" xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2 cursor-pointer w-3 h-3"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
                                         </div>
                                     </th>
                                     <!-- Next Control Col-->
                                     <th  class="text-sm font-medium text-gray-900 px-3  py-4 text-left">
                                         <div class="flex items-center">
                                             Prochain contrôle
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
+                                            <svg @click="sort('next_control')" xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512" class="ml-2 cursor-pointer w-3 h-3"><path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/></svg>
                                         </div>
                                     </th>
                                     <th  class="text-sm font-medium text-gray-900 px-3  py-4 text-left">
@@ -319,7 +323,7 @@ import DataCard from '@/Components/DataCard.vue';
                                 </tr>
                             </thead>
                             <tbody >
-                                <tr v-for="(material,key) in materials" :key="key" >
+                                <tr v-for="(material,key) in sortedMaterials" :key="key" >
                                     <td class="px-3 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                         <Checkbox :value="selectAll"/>
                                     </td>
@@ -389,7 +393,9 @@ import axios from "axios";
 export default {
     data(){
         return {
-            materials:[]
+            materials:[],
+            currentSort:'name',
+            currentSortDir:'asc'
         }
     },
     mounted(){
@@ -412,6 +418,23 @@ export default {
                 }).catch(error=>{
                     console.log(error)
                 })
+        },
+        sort:function(s){
+            if(s === this.currentSort) {
+                this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+            }
+            this.currentSort = s;
+        }
+    },
+    computed:{
+        sortedMaterials:function() {
+        return this.materials.sort((a,b) => {
+            let modifier = 1;
+            if(this.currentSortDir === 'desc') modifier = -1;
+            if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+            if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+            return 0;
+        });
         }
     }
 }
