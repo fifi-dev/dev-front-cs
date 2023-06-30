8ccdafac91bf3273c2c63c32e848e270393d7c70
Fideline <fideline_bzn@yahoo.com>
2023-06-30 19:10:19 +0200

    logo & header details

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -72,6 +72,39 @@ const logout = () => {
                                 </button>
                             </div>
                             <div class="hidden ml-0   sm:flex sm:items-center pr-8 ">
+                                <!-- Languages-->
+                                <div class=" max-md:hidden relative w-fit mr-2 ">
+                                    <Dropdown align="right" width="48">
+                                        <template #trigger>
+                                        <span>
+                                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent  font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                                    <img src="/assets/img/uk_flag.png" alt="Logo Charlie Solutions" class="w-8"/>
+                                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                                    </svg>
+                                                </button>
+                                            </span>
+                                        </template>
+
+                                        <template #content>
+                                            <!-- Language option -->
+                                            <DropdownLink>
+                                                <div class="flex items-center">
+                                                    <img src="/assets/img/uk_flag.png" alt="drapeau uk" class="w-8 mr-3"/>
+                                                    <p>Anglais</p>
+                                                </div>
+                                            </DropdownLink>
+
+                                            <!-- Language option -->
+                                            <DropdownLink>
+                                                <div class="flex items-center">
+                                                    <img src="/assets/img/french_flag.png" alt="drapeau uk" class="w-8 mr-3"/>
+                                                    <p>Français</p>
+                                                </div>
+                                            </DropdownLink>
+                                        </template>
+                                    </Dropdown>
+                                </div>
                                 <!-- Notifications-->
                                 <div class=" max-md:hidden relative w-fit mr-2 ">
                                     <Dropdown align="right" width="48">
