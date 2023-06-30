2dc684ff10f207c57cb653d893ebd58630f5cbfc
Fideline <fideline_bzn@yahoo.com>
2023-06-30 14:49:36 +0200

    api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MaterialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware('api')->group(function () {
+    Route::resource('material', MaterialController::class);
 });
